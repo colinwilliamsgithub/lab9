ef807526f1681aefbf37fb3ac8931c971ed9f7ac
colinwilliamsgithub <colinwilliams7890@gmail.com>
2023-04-21 11:24:54 +0000

    lab9

---
+++ src/app/status/status.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-status',
+  templateUrl: './status.page.html',
+  styleUrls: ['./status.page.scss'],
+})
+export class StatusPage implements OnInit {
+  myStatus:string = "";
+  constructor(private storage:Storage, private navCtrl:NavController) { }
+
+  async onSave() {
+    await this.storage.create();
+    await this.storage.set("status", this.myStatus);
+    await this.navCtrl.navigateBack('/home');
+  }
+  async ionViewWillEnter() {
+    await this.storage.create();
+    this.myStatus = await this.storage.get('status');
+  }
+  ngOnInit() {
+  }
+
+}
